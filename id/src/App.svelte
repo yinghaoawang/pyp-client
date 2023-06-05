3fbcaa510c5a3dc7034fc088f1cd709af18fc207
Yinghao Wang <yinghao.a.wang@gmail.com>
2023-06-05 12:11:53 -0700

    add login form w/ post request

--- id/src/App.svelte
+++ id/src/App.svelte
@@ -1,47 +1,117 @@
 <script>
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  import {
+    useForm,
+    validators,
+    HintGroup,
+    Hint,
+    email,
+    required
+  } from 'svelte-use-form';
+
+  import axios from 'axios';
+
+  const form = useForm();
+  const submitForm = (evt) => {
+    const formData = new FormData(evt.target);
+    const data = {};
+    for (let field of formData) {
+      const [key, value] = field;
+      data[key] = value;
+    }
+
+    axios
+      .post('http://localhost:3000/login', data)
+      .then((res) => {
+        console.log('Everything went good: ', res.data);
+      })
+      .catch((err) => {
+        console.error(err);
+        if (err.code === 'ERR_BAD_REQUEST') {
+          alert('Invalid credentials');
+        } else {
+          alert('Something went wrong: ' + err.code);
+        }
+      });
+  };
 </script>
 
-<main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+<div class="outer-container">
+  <div class="content-container">
+    <form use:form on:submit|preventDefault={submitForm}>
+      <h1>Login</h1>
+
+      <div class="form-input">
+        <label for="email">Email</label>
+        <input
+          type="email"
+          name="email"
+          id="email"
+          use:validators={[required, email]}
+        />
+        <HintGroup for="email">
+          <Hint on="required">Required</Hint>
+          <Hint on="email" hideWhenRequired>Email is not valid</Hint>
+        </HintGroup>
+      </div>
+
+      <div class="form-input">
+        <label for="password">Password</label>
+        <input
+          type="password"
+          name="password"
+          id="password"
+          use:validators={[required]}
+        />
+        <Hint for="password" on="required">Required</Hint>
+      </div>
+
+      <div class="form-input">
+        <button disabled={!$form.valid}>Login</button>
+      </div>
+    </form>
+    <details>
+      <pre>{JSON.stringify($form, null, ' ')}</pre>
+    </details>
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
-</main>
+</div>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+  :global(.touched:invalid) {
+    border-color: red;
+    outline-color: red;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
+
+  .form-input {
+    margin-bottom: 15px;
   }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
+
+  label {
+    display: block;
   }
-  .read-the-docs {
-    color: #888;
+
+  .outer-container {
+    max-width: 350px;
+    margin: 0 auto;
+  }
+
+  .content-container {
+    width: 100%;
+    background: #444;
+    padding: 40px;
+  }
+
+  h1 {
+    margin-top: 0;
+  }
+
+  input {
+    width: 100%;
+  }
+
+  pre {
+    max-height: 200px;
+    overflow: auto;
+    background-color: black;
+    padding: 20px;
   }
 </style>
