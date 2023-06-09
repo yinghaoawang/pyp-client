c5dd4ffab20c6cb6bdf3ac1b8a10b54b3d5d3490
Yinghao Wang <yinghao.a.wang@gmail.com>
2023-06-09 10:45:43 -0700

    refactor loading code in lobby directory

--- game-client/src/scenes/LobbyDirectory.js
+++ game-client/src/scenes/LobbyDirectory.js
@@ -7,8 +7,6 @@ const COLOR_PRIMARY = 0x4e342e;
 const COLOR_LIGHT = 0x7b5e57;
 const COLOR_DARK = 0x260e04;
 
-let lobbies = [];
-
 class LobbyDirectory extends Phaser.Scene {
   constructor() {
     super({ key: 'LobbyDirectory' });
@@ -19,85 +17,35 @@ class LobbyDirectory extends Phaser.Scene {
   preload() {}
 
   create(data) {
-    lobbies = [];
+    this.lobbies = [];
     this.loadingText = this.add
       .text(getCenter(this).x, getCenter(this).y, 'Joining Lobby')
       .setOrigin(0.5, 0.5)
       .setDepth(4)
       .setVisible(false);
 
-    this.isLoadingJoinLobby = false;
-    this.isLoadingLobbies = true;
+    this.loadingSet = new Set(['getLobbies']);
 
-    const scrollablePanel = this.rexUI.add
-      .scrollablePanel({
-        x: 400,
-        y: 265,
-        width: 450,
-        height: 500,
+    const scrollablePanel = createScrollablePanel(this);
 
-        scrollMode: 0,
-
-        background: this.rexUI.add.roundRectangle(
-          0,
-          0,
-          2,
-          2,
-          10,
-          COLOR_PRIMARY
-        ),
-
-        panel: {
-          child: this.rexUI.add.fixWidthSizer({
-            space: {
-              left: 3,
-              right: 3,
-              top: 3,
-              bottom: 3,
-              item: 8,
-              line: 8
-            }
-          }),
-
-          mask: {
-            padding: 1
-          }
-        },
-
-        slider: {
-          track: this.rexUI.add.roundRectangle(0, 0, 20, 10, 10, COLOR_DARK),
-          thumb: this.rexUI.add
-            .roundRectangle(0, 0, 0, 0, 13, COLOR_LIGHT)
-            .setInteractive({ cursor: 'pointer' })
-        },
-
-        space: {
-          left: 10,
-          right: 10,
-          top: 10,
-          bottom: 10,
-
-          panel: 10
-        }
-      })
-      .layout();
-
-    updatePanel(this, scrollablePanel, lobbies);
+    updatePanel(this, scrollablePanel, this.lobbies);
 
     const game = this;
     setTimeout(() => {
-      lobbies =   [{
-        name: 'jkKJSKLjkl234lkjzJK',
-        host: 'Jojo',
-        users: [1, 2, 3, 4, 5]
-      },
-      {
-        name: 'doko',
-        host: 'coco',
-        users: [8, 42]
-      }];
-      this.isLoadingLobbies = false;
-      updatePanel(game, scrollablePanel, lobbies)
+      this.lobbies = [
+        {
+          name: 'jkKJSKLjkl234lkjzJK',
+          host: 'Jojo',
+          users: [1, 2, 3, 4, 5]
+        },
+        {
+          name: 'doko',
+          host: 'coco',
+          users: [8, 42]
+        }
+      ];
+      this.loadingSet.delete('getLobbies');
+      updatePanel(game, scrollablePanel, this.lobbies);
     }, 1000);
 
     this.add.text(5, 580, 'Join or create a lobby');
@@ -112,22 +60,86 @@ class LobbyDirectory extends Phaser.Scene {
       .buttons({ x: 400, y: 550 })
       .add(createButton(this, 'Create Lobby'))
       .layout()
-      .setInteractive({ cursor: 'pointer' });
+      .setInteractive({ cursor: 'pointer' })
+      .on('pointerdown', () => {
+        game.loadingSet.add('createLobby');
+
+        setTimeout(() => {
+          game.loadingSet.delete('createLobby');
+          game.scene.start('LobbyScene');
+        }, 1000);
+      });
   }
 
   update(time, delta) {
-    if (this.isLoadingLobbies) {
+    if (this.loadingSet.has('getLobbies')) {
       this.loadingText.setVisible(true).setText('Loading Lobbies');
-    } else if (this.isLoadingJoinLobby) {
+    } else if (this.loadingSet.has('createLobby')) {
+      this.loadingText.setVisible(true).setText('Creating Lobby');
+    } else if (this.loadingSet.has('joinLobby')) {
       this.loadingText.setVisible(true).setText('Joining Lobby');
     } else {
       this.loadingText.setVisible(false);
     }
   }
+
+  isLoading() {
+    return this.loadingSet.size > 0;
+  }
 }
+
+const createScrollablePanel = function (game) {
+  return game.rexUI.add
+    .scrollablePanel({
+      x: 400,
+      y: 265,
+      width: 450,
+      height: 500,
+
+      scrollMode: 0,
+
+      background: game.rexUI.add.roundRectangle(0, 0, 2, 2, 10, COLOR_PRIMARY),
+
+      panel: {
+        child: game.rexUI.add.fixWidthSizer({
+          space: {
+            left: 3,
+            right: 3,
+            top: 3,
+            bottom: 3,
+            item: 8,
+            line: 8
+          }
+        }),
+
+        mask: {
+          padding: 1
+        }
+      },
+
+      slider: {
+        track: game.rexUI.add.roundRectangle(0, 0, 20, 10, 10, COLOR_DARK),
+        thumb: game.rexUI.add
+          .roundRectangle(0, 0, 0, 0, 13, COLOR_LIGHT)
+          .setInteractive({ cursor: 'pointer' })
+      },
+
+      space: {
+        left: 10,
+        right: 10,
+        top: 10,
+        bottom: 10,
+
+        panel: 10
+      }
+    })
+    .layout();
+};
 
 const updatePanel = function (game, panel, lobbies) {
   const sizer = panel.getElement('panel');
+
+  if (sizer == null) return null;
 
   sizer.clear(true);
   for (const lobby of lobbies) {
@@ -141,10 +153,10 @@ const updatePanel = function (game, panel, lobbies) {
         .setTint(Phaser.Math.Between(0, 0x999999))
         .setInteractive({ cursor: 'pointer' })
         .on('pointerdown', () => {
-          game.isLoadingJoinLobby = true;
+          game.loadingSet.add('joinLobby');
 
           setTimeout(() => {
-            game.isLoadingJoinLobby = false;
+            game.loadingSet.delete('joinLobby');
             game.scene.start('LobbyScene');
           }, 1000);
         })
